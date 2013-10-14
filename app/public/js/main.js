a7227ed5c539153cc2ed0489ba44458b837c160e
Florent Jaby <fjaby@octo.com>
2013-10-14 16:38:35 +0200

    write result to div.answer

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,6 +1,10 @@
 $(function () {
+  var answer = $('.answer');
   $('#execute').click(function () {
     var code = $('#jscode').val();
+    var write = function (result) {
+      answer.html(result)
+    };
     eval(code);
   });
 });
