94c46ddac4d597c38a47d1ae86d21186d5013103
Pierrick <pierrick.boutruche@gmail.com>
2013-10-23 20:21:51 +0200

    created step2

---
+++ app/main.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var vm = require('vm');
+
+var server = express();
+
+server.configure(function () {
+  server.use(express.logger('dev'));
+  server.use(express.static('public'));
+  server.use(express.bodyParser());
+});
+
+server.post('/postCode', function(req, res, next) {
+  var code = req.body.code;
+  try  {
+    var result = vm.runInNewContext(code);
+  } catch(err) {
+    next(err);
+  }
+  res.end(String(result));
+});
+
+var port = (process.env.PORT ? process.env.PORT : 8000);
+server.listen(port).listen(function () {
+  console.log("Node Server running on port " + port);
+});
+
