40e85f255f7e3084c616fea8c04cc716b949beaa
Pierrick <pierrick.boutruche@gmail.com>
2013-11-06 17:53:03 +0100

    added tests for step 3

---
+++ app/test/step3.js
@@ -0,0 +1,17 @@
+var io = require('socket.io-client');
+var expect = require('chai').expect;
+
+describe('Le serveur', function(){
+	it("distribue le code reçu d'un des clients", function(done){
+		// give
+		var emetteur = io.connect('http://localhost:8000');
+		var recepteur = io.connect('http://localhost:8000', {'force new connection': true});
+		// when
+		emetteur.emit('sendJS', {code: '1+1'});
+		// then
+		recepteur.on('broadcastJS', function(data){
+			//expect(data.code).to.be.equal('1+1');
+			done();
+		});
+	});
+});
