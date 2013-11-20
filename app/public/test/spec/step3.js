60c62186b5097b096764d07be690c57e69926cab
Pierrick <pierrick.boutruche@gmail.com>
2013-11-20 12:17:23 +0100

    test A for step3.0 browser

--- app/public/test/spec/step3.js
+++ app/public/test/spec/step3.js
@@ -1,33 +1,26 @@
-var io = require('socket.io-client');
-var expect = require('chai').expect;
-
 describe("L'application", function(){
-	// it("envoie la fonction sur un autre browser", function(){
-	// 	var recepteur = io.connect('http://localhost:8000');
+	it("envoie la fonction sur un autre browser", function(done){
+		var emetteur = io.connect('http://localhost:8000');
 
-	// 	var textarea = $('#jscode');
-	// 	var exec = $('#execute');
-	// 	var getJS = $('#getJS');
+		var textarea = $('#jscode');
+		var exec = $('#execute');
 
-	// 	var hasBeenCalled = false;
+		emetteur.emit = function(socket, data){
+			expect(socket).to.be.equal('sendJS');
+			expect(data).to.be.an('object');
+			expect(data).to.include.keys('code');
+			expect(data.code).to.be.equal('test');
+			done();
+		};	
 
-	// 	var myTestFunction = function (param){
- //            expect(param).to.equal(8);
- //            hasBeenCalled = true;
- //        };
- //        window.myTestFunction = myTestFunction;
- //        textarea.val('myTestFunction(8)');
- //        exec.trigger('click');
-
- //        recepteur.on('broadcastJS', function(){
-
- //        });
-	// });
+        textarea.val('test');
+        exec.trigger('click');
+	});
 
 	it("reçoit le code et l'exécute", function(){
 		var emetteur = io.connect('http://localhost:8000');
-
 		var getJS = $('#getJS');
+		var hasBeenCalled = false;
 
 		var myTestFunction = function (param){
             expect(param).to.equal(8);
