2a2765e9278887df298f059ac6f4a871dcc2baca
Pierrick <pierrick.boutruche@gmail.com>
2013-12-11 10:46:39 +0100

    clean code for client.js

--- app/public/js/client.js
+++ app/public/js/client.js
@@ -1,45 +1,56 @@
 $(function () {
+	// Connection to Socket.IO
 	var socket = io.connect('http://localhost:8000');
 
-	socket.on('sessionID', function(data){
-		$('#sessionID').html('sessionID : ' + data.sessionID);
-	});
 
-	$('#execute').click(function () {
-		socket.emit('sendJS', { titre: $('#name').val(), dataSet: $('#DataSet').val(),
-								map: $('#Map').val(), reduce: $('#Reduce').val() });
-	});
-	
-	
+	// Listeners
+		// UI
+	socket.on('sessionID', displaySessionID);
+	socket.on('nbUsers', displayUsers);
+	socket.on('newProject', updateProjectsList);
+	socket.on('hereIsTheResult', displayResult);
+		// Calculations
+	socket.on('sendChunk', doTheMaths);
 
-	socket.on('sendChunk', function(data){
-		console.log(data);
-		var results = [];
-		for(var index in data.dataSet)
-		{
-			var a = data.dataSet[index];
-			var result = eval(data.map);
-			results.push(result);
-		}
-		socket.emit('sendChunkResults', {'owner': data.owner, 'results': results});
-	});
 
-	socket.on('nbUsers', function (data) {
-		$('#nbUsers').text("Number of connected users: "+data);
-	});
-
-	socket.on('newProject', function (data) {
-		if(Object.keys(data).length > 0)
-		{
-			$.each(data, function(key, value){
-				$(value).each(function(){
-					$('#projects').append('<div class="project">'+this.titre+'</div>');
-				});
-			});
-		}
-	});
-
-	socket.on('hereIsTheResult', function(result){
-		alert(result);
+	// Actionners 
+	$('#execute').click(function() {
+		socket.emit('sendJS', { titre: $('#name').val(), dataSet: $('#DataSet').val(), map: $('#Map').val(), reduce: $('#Reduce').val() });
 	});
 });
+
+
+var displaySessionID = function(data){
+	$('#sessionID').text('sessionID : ' + data.sessionID);
+};
+
+var displayUsers = function(data){
+	$('#nbUsers').text("Number of connected users: "+data);
+};
+
+var displayResult =  function(result){
+	alert(result);
+};
+
+var updateProjectsList = function(data) {
+	if(Object.keys(data).length > 0)
+	{
+		$.each(data, function(key, value){
+			$(value).each(function(){
+				$('#projects').append('<div class="project">'+this.titre+'</div>');
+			});
+		});
+	}
+};
+
+var doTheMaths = function(data){
+	console.log(data);
+	var results = [];
+	for(var index in data.dataSet)
+	{
+		var a = data.dataSet[index];
+		var result = eval(data.map);
+		results.push(result);
+	}
+	socket.emit('sendChunkResults', {'owner': data.owner, 'results': results});
+};
