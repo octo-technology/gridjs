c4fc7029be0ae5c0bdbc9da131dcc736160381f5
Henri Tremblay <henri.tremblay@gmail.com>
2014-03-22 09:28:27 +0100

    Add Grunt just to use LiveReload

---
+++ app/Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    connect: {
+      options: {
+        hostname: 'localhost',
+        port: 9001,
+        livereload: 35729
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: 'public'
+        }
+      }
+    },
+
+    watch: {
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          '{,*/}*.html',
+          '{,*/}*.css',
+          '{,*/}*.js'
+        ]
+      }
+    }
+
+  });
+
+  grunt.registerTask('serve', [
+    'connect:livereload',
+    'watch'
+  ]);
+
+};
